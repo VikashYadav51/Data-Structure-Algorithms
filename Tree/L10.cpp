22f5fced24ecde62dfff0943274a00cecf0d36ef
Vikash <vikashyadav5131@gmail.com>
2026-01-02 16:04:54 +0530

    Implement binary search tree construction and level order traversal

---
+++ Tree/L10.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int element){
+        this->data = element;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+
+Node*  construct_BST(Node* root, int data){
+    if(root == nullptr){
+        root = new Node(data);
+        return root;
+    }
+
+    if(data < root->data){
+        root->left = construct_BST(root->left, data);
+    }
+
+    else{
+        root->right = construct_BST(root->right, data);
+    }
+
+    return root;
+}
+
+void takeInput(Node* &root){
+    int data;
+    cout <<"Enter the data "<< endl;
+    cin>> data;
+
+    if(data != -1){
+        root = construct_BST(root, data);
+        takeInput(root);
+    }
+
+    return ;
+}
+
+void print_LOT(Node* root){
+    if(root == nullptr){
+        return ;
+    }
+
+    queue<Node* > q1;
+    q1.push(root);
+
+    while(!q1.empty()){
+        int size = q1.size();
+
+        for(int i = 0; i < size; i++){
+            Node* node = q1.front();
+            q1.pop();
+
+            cout << node->data <<",  ";
+
+            if(node->left){
+                q1.push(node->left);
+            }
+
+            if(node->right){
+                q1.push(node->right);
+            }
+        }
+
+        cout << endl ;
+    }
+}
+
+
+
+
+
+int main(){
+    Node* root = nullptr;
+
+    takeInput(root);
+
+    print_LOT(root);
+
+}
