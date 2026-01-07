f3e6b11fb094dbb89e78672c263f3d3387509386
Vikash <vikashyadav5131@gmail.com>
2026-01-07 17:11:38 +0530

    Check given tree is valid BST or not

--- Tree/L11.cpp
+++ Tree/L11.cpp
@@ -118,6 +118,24 @@ void k_smallest_element(Node* root, int k, int &count, int &value){
     k_smallest_element(root->right, k, count, value);
 }
 
+Node* LCA(Node* root, int n1, int n2){
+    if(root == nullptr){
+        return nullptr;
+    }
+    
+    if((n1 < root->data && n2 > root->data) || (n1 > root->data && n2 < root->data)){
+        return root;
+    }
+
+    if(n1 < root->data && n2 < root->data){
+        return LCA(root->left, n1, n2);
+    }
+
+    else{
+        return LCA(root->right, n1, n2);
+    }
+}
+
 
 
 
