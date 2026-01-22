924300b48ccb2b466884ea525a258b5e9ba23779
Vikash <vikashyadav5131@gmail.com>
2026-01-23 00:26:39 +0530

    change binary to decimal conversion logic

--- Basic_DSA/L2.cpp
+++ Basic_DSA/L2.cpp
@@ -2,19 +2,36 @@
 using namespace std;
 
 int main(){
-    int n;
-    cout <<"Enter the value of n "<< endl;
-    cin >> n;
+    // int n;
+    // cout <<"Enter the value of n "<< endl;
+    // cin >> n;
 
-    int ans = 0;
-    int i = 1;
+    // int ans = 0;
+    // int i = 1;
 
-    while(n != 0){
-        int bits = n % 2;
-        ans = bits * i + ans;
-        i = i * 10;
-        n = n / 2;
-    }
+    // while(n != 0){
+    //     int bits = n % 2;
+    //     ans = bits * i + ans;
+    //     i = i * 10;
+    //     n = n / 2;
+    // }
 
-    cout << "Binary number of n "<< ans << endl;
+    // cout << "Binary number of n "<< ans << endl;
+
+
+    // int n;
+    // cout <<"Enter the value of n "<< endl;
+    // cin >> n;
+
+    // int i = 1;
+    // int ans = 0;
+
+    // while(n != 0){
+    //     int bits = n % 10;
+    //     ans = ans + bits * i;
+    //     i = i*2;
+    //     n = n  / 10;
+    // }
+
+    // cout <<"Decimal number is "<< ans << endl;
 }
