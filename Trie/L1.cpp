c75076a086279784f5195eb9066fceff58acbdd7
Vikash <vikashyadav5131@gmail.com>
2026-01-30 16:14:38 +0530

    write the algo of insertion , Serching, deletion

--- Trie/L1.cpp
+++ Trie/L1.cpp
@@ -5,19 +5,98 @@ class TrieNode{
     public:
     char data;
     TrieNode* children[26];
-    bool isterminal;
+    bool isTerminal;
 
     TrieNode(char ch){
         this->data = ch;
-        this->isterminal = false;
+        this->isTerminal = false;
         for(int i = 0; i < 26; i++){
             this->children[i] = nullptr;
         }
     }
 };
 
+class Trie{
+    public:
+    TrieNode* root;
+
+    Trie(){
+        root = new TrieNode('\0');
+    }
+
+    void insert_utils(TrieNode* root, string word){
+        if(word.length() == 0){
+            root->isTerminal = true;
+            return ;
+        }
+        
+        int index = word[0] - 'A';
+
+        if(root->children[index] == nullptr){
+            root->children[index] = new TrieNode(word[0]);
+        }
+
+        root = root->children[index];
+        
+        insert_utils(root, word.substr(1));
+        return ;
+    }
+
+    void insert_word(string s){
+        insert_utils(root, s);
+    }
+
+
+    bool search_word(string s1){
+        TrieNode* child = root;
+        int i = 0;
+        while(i < s1.length()){
+            int index = s1[i] - 'A';
+
+            if(child->children[index] == nullptr){
+                return false;
+            }
+
+            child = child->children[index];
+            i++;
+
+        }
+
+        return child->isTerminal;
+    }
+
+    void remove_element(string s1){
+        TrieNode* child = root;
+        int i = 0;
+
+        while(i < s1.length()){
+            int index = s1[i]  - 'A';
+            if(child->children[index] != nullptr){
+                child = child->children[index];
+            }
+            i++;
+        }
+
+        if(child->isTerminal){
+            child->isTerminal = false;
+        }
+
+        return ;
+    }
+};
+
 
 
 int main(){
+    Trie* t1 = new Trie();
+    t1->insert_word("ABCD");
+
+    bool ans = t1->search_word("ABCDE");
+    cout << " ans "<< ans  << endl;
+
+    t1->remove_element("ABCD");
+
+    bool ans1 = t1->search_word("ABCD");
+    cout << " ans "<< ans1  << endl;
 
 }
