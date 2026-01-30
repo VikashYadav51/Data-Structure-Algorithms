dd1374875d09dc988a2caa7eca021bce32fd2a33
Vikash <vikashyadav5131@gmail.com>
2026-01-30 14:24:07 +0530

    Implement of Trie Class

--- Trie/L1.cpp
+++ Trie/L1.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 
+class TrieNode{
+    public:
+    char data;
+    TrieNode* children[26];
+    bool isterminal;
+
+    TrieNode(char ch){
+        this->data = ch;
+        this->isterminal = false;
+        for(int i = 0; i < 26; i++){
+            this->children[i] = nullptr;
+        }
+    }
+};
+
+
+
 int main(){
-    
+
 }
