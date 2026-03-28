fa1bf73c118bbc99e417308060cda70b44be1b4a
Vikash <vikashyadav5131@gmail.com>
2026-03-28 15:54:04 +0530

    define the algo of nearest celll having

--- Graph/L10.cpp
+++ Graph/L10.cpp
@@ -1,60 +1,63 @@
 #include<iostream>
-#include<vector>
 #include<queue>
+#include<vector>
 
 using namespace std;
 
-// Distance of nearest cell having 1
+int main(){
+    int n;
+    cout <<"Enter the value of n "<< endl;
+    cin >> n;
 
-vector<vector<int>> nearest(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    
-    queue<pair<pair<int, int>, int> > q1;
-    
-    vector<vector<bool> > visited(n, vector<bool>(m, 0));
-    
-    vector<vector<int> > nums(n, vector<int>(m, 0));
-    
+    int m;
+    cout <<"Enter the value of m "<< endl;
+    cin >> m;
+
+    vector<vector<int> > matrix(n, vector<int> (m, 0));
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            if(grid[i][j] == 1){
-                visited[i][j] = true;
+            cin >> matrix[i][j] ;
+        }
+    }
+
+    queue<pair< pair<int, int>, int> > q1;
+    vector<vector<int> > visited(n, vector<int> (m, 0));
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(matrix[i][j] == 1){
                 q1.push({{i, j}, 0});
+                visited[i][j] = true;
+                matrix[i][j] = 0;
             }
         }
     }
-    
+
     while(!q1.empty()){
         int row = q1.front().first.first;
         int col = q1.front().first.second;
         int dist = q1.front().second;
-        
+
         q1.pop();
-        
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        
-        int size1 = grid.size();
-        int size2 = grid[0].size();
-        
+
+        int drwo[] = {};
+        int dcol[] = {};
+
+        int rowSize = matrix.size();
+        int colSize = matrix[0].size();
+
         for(int i = 0; i < 4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            
-            if(nrow >= 0 && nrow < size1 && ncol >= 0 && ncol < size2){
-                if(!visited[nrow][ncol]){
-                    q1.push({{nrow, ncol}, dist+1});
-                    nums[nrow][ncol] = dist + 1;
-                    visited[nrow][ncol] = true;
-                }
+            int n = row + i;
+            int m = row + i;
+
+            if(n >= 0 && n < rowSize && m >= 0 && m < colSize && !visited[n][m] ){
+                q1.push({{n, m}, dist+1});
+                visited[n][m] = true;
+                matrix[n][m] = dist + 1;
             }
         }
     }
-    
-    return nums;
-}
 
-int main(){
-
+    return 0;
 }
